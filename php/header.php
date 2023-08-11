1440b33ed3341b0c81e62c78c02b9b768bc09d21
Wade-CQU <wade.cole@cqumail.com>
2023-08-11 16:47:02 +1000

    Fixed nav bar to match wireframe
    Fixed nav bar to match wireframe.

--- php/header.php
+++ php/header.php
@@ -1,13 +1,9 @@
 <header>
-    <img class="menu-icon" src="https://cdn-icons-png.flaticon.com/512/2516/2516745.png">
-    <img class="nav-logo" src="https://moodle.cqu.edu.au/pluginfile.php/1/core_admin/logocompact/300x300/1688017676/cqu-logo.png"  alt="Moodle">
-    <nav >
-        <ul class="nav-links">
-            <li><a class="nav-element" href="">COURSES</a></li>
-            <li><a class="nav-element" href="">ASSESSMENT</a></li>
-            <li><a class="nav-element" href="">SUPPORT</a>  </li>
-        </ul>
-    </nav>
+    <img class="nav-logo" src="assets/Logo.svg"  alt="Moodle">
+    <div class="term-xp-label">Term XP:</div>
+    <div class="term-xp-bar">
+        <div class="term-xp-progress"></div>
+    </div>
     <img class="profile-icon" src="https://cdn-icons-png.flaticon.com/512/64/64572.png">
 </header>
 <div class="divider"></div>
