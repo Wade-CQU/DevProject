b3a906774fb7bcec1c5c074287fb12e3f6d20f7a
JackPashley21 <50433738+JackPashley21@users.noreply.github.com>
2023-09-07 21:09:32 +1000

    Student Assignment done. Teacher assignment startd

--- upload.php
+++ upload.php
@@ -6,66 +6,68 @@ include("php/dbConnect.php");
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="/devproject/css/default.css" rel="stylesheet" />
-        <link href="/devproject/css/term.css" rel="stylesheet" />
-        <title></title>
-    </head>
-    <body>
-        <?php require("php/header.php"); ?>
 
-        <?php 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/devproject/css/default.css" rel="stylesheet" />
+  <link href="/devproject/css/term.css" rel="stylesheet" />
+  <title></title>
+</head>
 
-        //TODO's
-        //$unitId = $_GET['unitId'];
-        //$studentId = $_GET['userId'];
-        $assignmentId = $_GET['assignmentId'];
-        echo "Assignment ID = " . $assignmentId;
-        $unitId = 1;
-        $userId = 1;
-        //$assignmentId = 1;
-        $target_dir = "Assignments/$unitId/$userId/$assignmentId/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+<body>
+  <?php require("php/header.php"); ?>
 
-
-
-        if (is_dir("Assignments/") == false){
-          mkdir("Assignments/", 0777);
-          echo "here 1 ";
-        }
-        if (is_dir("Assignments/$unitId/") == false){
-          mkdir("Assignments/$unitId/", 0777);
-          echo "here 2 ";
-        }
-        if(is_dir("Assignments/$unitId/$userId/") == false){
-            mkdir("Assignments/$unitId/$userId/", 0777);
-            echo "here 3 ";
-        }
-        if(is_dir("Assignments/$unitId/$userId/$assignmentId/") == false){
-            mkdir("Assignments/$unitId/$userId/$assignmentId/", 0777);
-            echo "here 4 ";
-        } else{
-          echo "here else ";
-        }
-
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-          // if everything is ok, try to upload file
-          } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-              echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.\n";
-            } else {
-              echo "Sorry, there was an error uploading your file.\n";
-            }
-          }
-
-        ?>
+  <?php
+  $unitId = $_GET['unitId'];
+  $studentId = $_GET['userId'];
+  $assignmentId = $_GET['assignmentId'];
+  echo "Assignment ID = " . $assignmentId . " ----- ";
+  echo "Unit ID = " . $unitId . " ----- ";
+  //$unitId = 1;
+  //$userId = 1;
+  //$assignmentId = 1;
+  $target_dir = "Assignments/$unitId/$userId/$assignmentId/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 
-    </body>
+
+  if (is_dir("Assignments/") == false) {
+    mkdir("Assignments/", 0777);
+    echo "here 1 ";
+  }
+  if (is_dir("Assignments/$unitId/") == false) {
+    mkdir("Assignments/$unitId/", 0777);
+    echo "here 2 ";
+  }
+  if (is_dir("Assignments/$unitId/$userId/") == false) {
+    mkdir("Assignments/$unitId/$userId/", 0777);
+    echo "here 3 ";
+  }
+  if (is_dir("Assignments/$unitId/$userId/$assignmentId/") == false) {
+    mkdir("Assignments/$unitId/$userId/$assignmentId/", 0777);
+    echo "here 4 ";
+  } else {
+    echo "here else ";
+  }
+
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.\n";
+    } else {
+      echo "Sorry, there was an error uploading your file.\n";
+    }
+  }
+
+  ?>
+
+
+</body>
+
 </html>
