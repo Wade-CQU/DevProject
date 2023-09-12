b1a4a7a121d2a31093f77aa9716b627fd6d504af
JackPashley21 <50433738+JackPashley21@users.noreply.github.com>
2023-09-12 18:51:31 +1000

    Jackisdent 2

--- php/assignmentGrade.php
+++ php/assignmentGrade.php
@@ -4,15 +4,19 @@ include("dbConnect.php");
 
 //Get post data
 
-echo "Student ID: = " . $_POST['userId'];
+echo "  Student ID: = " . $_POST['userId'];
+echo "  AssignmentId: = " . $_POST['assignmentId'];
+echo "  UnitId = " . $_POST['unitId'];
+echo "  Grade = " . $_POST['grade'];
 
-$studentId = $_POST['userId'];
+$userId = $_POST['userId'];
 $assignmentId = $_POST['assignmentId'];
 $unitId = $_POST['unitId'];
 $grade = $_POST['grade'];
 
 //SQL to make grade
 $sql = "UPDATE submission SET grade = $grade WHERE (assignmentsId = $assignmentId AND userId = $userId)";
+echo $sql;
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $stmt->close();
